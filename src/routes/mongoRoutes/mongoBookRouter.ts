83d9d75e61f5a67929dc6ab013829c054c0a5ac0
William Omø <williamjean.k.o@gmail.com>
2023-10-31 15:10:33 +0100

    Added Rasmus' C

--- src/routes/mongoRoutes/mongoBookRouter.ts
+++ src/routes/mongoRoutes/mongoBookRouter.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import logger from '../../other_services/winstonLogger';
 import Book from '../../other_services/mongoSchemas/bookSchema';
 import User from '../../other_services/mongoSchemas/userDataSchema';
+import bcrypt from 'bcrypt';
 
 const router = express.Router();
 router.use(express.json());
@@ -150,6 +151,40 @@ async function updateUserMongo(id:string, body:any) {
         return user;
     } catch (error) {
         logger.error("Error in updating user: [Mongo updateUser, 2]", error);
+    }
+}
+
+router.post("/mongo/user/create", async (req, res) => {
+    try {
+        const result = await createUser(req.body.first_name, req.body.last_name, req.body.email, req.body.password);
+        res.status(200).json(result);
+    } catch (err:any) {
+        if (err.code == 409){
+            res.status(409).send(err.message);
+        } else {
+            res.status(500).send("Something went wrong while creating user");
+        }
+    }
+});
+
+async function createUser(first_name: string, last_name: string, email: string, password: string) {
+    try {
+        console.log("Creating user")
+        const hash = await bcrypt.hash(password, 10)
+        if (await User.exists({"user_data.email": email}) != null) {
+            throw {code: 409, message:"Email already exists"};
+        }
+        try {
+            const user = await User.create({ user_data: { first_name: first_name, last_name: last_name, email: email, password: hash}});
+            console.log("User created: ", user);
+            return user;
+        } catch (err) {
+            logger.error("Error in hashing password: [Mongo createUser, 1]",err);
+            throw err;
+        }
+    } catch (error) {
+        logger.error("Error in creating user",error);
+        throw error;
     }
 }
 
