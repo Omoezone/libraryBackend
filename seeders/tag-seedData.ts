dce529979e9e5bc68579f7e0ed195740de596b90
William Omø <williamjean.k.o@gmail.com>
2023-11-08 11:18:30 +0100

    added seed data sequelize

---
+++ seeders/tag-seedData.ts
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = {
+    up: async (queryInterface: any, Sequelize:any) => {
+        const tags = [
+            { title: 'Mystery', tag_description: 'Books that keep you guessing' },
+            { title: 'Fantasy', tag_description: 'Magical adventures and worlds' },
+            { title: 'Science Fiction', tag_description: 'Exploring the future' },
+            { title: 'Romance', tag_description: 'Love stories' },
+            { title: 'Thriller', tag_description: 'Edge-of-your-seat suspense' },
+            { title: 'Non-Fiction', tag_description: 'Factual and informative' },
+            { title: 'Historical', tag_description: 'Exploring the past' },
+            { title: 'Adventure', tag_description: 'Exciting journeys' },
+            { title: 'Biography', tag_description: 'Life stories of famous people' },
+            { title: 'Horror', tag_description: 'Spine-tingling scares' },
+            { title: 'Self-Help', tag_description: 'Improvement and motivation' },
+            { title: 'Comedy', tag_description: 'Funny and humorous' },
+            { title: 'Dystopian', tag_description: 'Grim future scenarios' },
+            { title: 'Cookbook', tag_description: 'Recipes and culinary tips' },
+            { title: 'Travel', tag_description: 'Exploring the world' },
+            { title: 'Poetry', tag_description: 'Expressive and poetic' },
+            { title: 'Art', tag_description: 'Creative and artistic' },
+            { title: 'Philosophy', tag_description: 'Deep thinking and ideas' },
+            { title: 'Business', tag_description: 'Entrepreneurship and finance' },
+            { title: 'Classics', tag_description: 'Timeless literary works' },
+            { title: 'Children', tag_description: 'Books for kids' },
+            { title: 'Science', tag_description: 'Exploring the natural world' },
+            { title: 'Technology', tag_description: 'Innovations and gadgets' },
+            { title: 'Music', tag_description: 'Rhythms and melodies' },
+            { title: 'Religion', tag_description: 'Spiritual and faith-based' },
+    ];
+
+    await queryInterface.bulkInsert('tags', tags, {});
+    },
+
+    down: async (queryInterface:any, Sequelize:any) => {
+        await queryInterface.bulkDelete('tags', null, {});
+    },
+};
---
+++ seeders/userName-seedData.ts
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const userNamesData = [
+      { first_name: 'John', last_name: 'Doe' },
+      { first_name: 'Jane', last_name: 'Smith' },
+      { first_name: 'Bob', last_name: 'Johnson' },
+      { first_name: 'Alice', last_name: 'White' },
+      { first_name: 'David', last_name: 'Brown' },
+      { first_name: 'Sarah', last_name: 'Davis' },
+      { first_name: 'Michael', last_name: 'Lee' },
+      { first_name: 'Emily', last_name: 'Anderson' },
+      { first_name: 'James', last_name: 'Martinez' },
+      { first_name: 'Olivia', last_name: 'Taylor' },
+      { first_name: 'Robert', last_name: 'Harris' },
+      { first_name: 'Sophia', last_name: 'Clark' },
+      { first_name: 'William', last_name: 'Moore' },
+      { first_name: 'Ava', last_name: 'Walker' },
+      { first_name: 'Joseph', last_name: 'Lopez' },
+      { first_name: 'Grace', last_name: 'Wilson' },
+      { first_name: 'Thomas', last_name: 'Young' },
+      { first_name: 'Ella', last_name: 'King' },
+      { first_name: 'Christopher', last_name: 'Garcia' },
+      { first_name: 'Mia', last_name: 'Allen' }
+    ];
+
+    await queryInterface.bulkInsert('user_names', userNamesData, {});
+
+    return Promise.resolve();
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('user_names', null, {});
+  },
+};
